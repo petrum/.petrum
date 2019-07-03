283bc839ed078cd46efc82dda9faefd0a69b15e4
Petru Marginean <petru.marginean@gmail.com>
2019-07-02 23:31:23 -0400

    misc

--- .startup.py
+++ .startup.py
@@ -3,5 +3,5 @@ pd.set_option('display.max_rows', 10)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 pd.set_option('display.expand_frame_repr', False)
-pandas.set_option('display.max_colwidth', 100)
+pd.set_option('display.max_colwidth', 100)
 
