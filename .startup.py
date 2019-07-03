200bc976ed13c49f551614f6651d9106f7d54d55
Petru Marginean <petru.marginean@gmail.com>
2019-07-03 18:37:18 -0400

    misc

--- .startup.py
+++ .startup.py
@@ -1,5 +1,6 @@
 import pandas as pd
-pd.set_option('display.max_rows', 30)
+
+pd.set_option('display.max_rows', 40)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 pd.set_option('display.expand_frame_repr', False)
